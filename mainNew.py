bef138e91d4d2a8f4abfdb0173866c119d962b20
felexion <philrexvictor@proton.me>
2025-11-27 23:24:06 +0800

    Shut Up Bot 1.1

--- mainNew.py
+++ mainNew.py
@@ -1,16 +1,30 @@
 import discord
 import random
 import os
+import logging
+import time
 from discord import app_commands
 from dotenv import load_dotenv
 
+logging.basicConfig(
+    level = logging.INFO,
+    format = '%(asctime)s - %(levelname)s - %(message)s',
+
+    handlers = [
+
+        logging.FileHandler("shutupbotlogs.log"),
+        logging.StreamHandler()
+    ]
+)
+
+logger = logging.getLogger("ShutUpBot")
+
 load_dotenv()
-VERSION = "v.1.0.3.rc6"
-VERSION_DESCRIPTION = "better helpcommand response"
-# TODO: add error messages
-# TODO: add avoiding replying in dms
-# TODO: add rate-limit handling if spammy
-# TODO: improve logging system
+VERSION = "v.1.1"
+VERSION_DESCRIPTION = "Logging system improved. Added spam cooldown."
+
+SPAM_COOLDOWN_SECONDS = 5.0
+user_spam_tracker = {}
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -22,41 +36,72 @@ class shutUpBot(discord.Client):
 
     async def setup_hook(self):
         await self.tree.sync()
+        logger.info("Command tree synced.")
 
 client = shutUpBot()
 
+@client.tree.error
+async def on_app_command_error(interaction: discord.Interaction, error: app_commands.AppCommandError):
+    if isinstance(error, app_commands.CommandOnCooldown):
+        await interaction.respond.send_message(f'Slow down, dude. Wait like {error.retry_after:.2f} seconds lol.')
+    else:
+        logger.error(f'Command error detected: {error}')
+        try:
+            await interaction.response.send_message("Something broke. SHUT UP and try again LATER.")
+        except:
+            pass
+
 @client.event
 async def on_ready():
-    print(f'Logged in as {client.user}!')
+    logger.info(f'Logged in as {client.user} (ID: {client.user.id})')
+    logger.info(f'Running version {VERSION}')
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
     
+    if message.guild is None:
+        return
+
+    current_time = time.time()
+    last_time = user_spam_tracker.get(message.author.id, 0)
+
+    if current_time - last_time < SPAM_COOLDOWN_SECONDS:
+        return
+    
+    triggered = False
+
     if message.reference and message.reference.resolved:
         replied_message = message.reference.resolved
         if replied_message.author == client.user:
             await message.reply("Just shut up!")
-            print("Bot was replied. Told user to shut up.")
+            logger.info(f'Replied to a user who replied to bot.')
             return    
 
-    if client.user in message.mentions:
+    elif client.user in message.mentions:
         await message.reply("Shut up!")
-        print("Bot was mentioned. Told user to shut up.")
-        return
+        logger.info("Replied to mention from a user.")
+        triggered = True
 
-    rand_val = random.randint(1, 100)
+    else:
+        rand_val = random.randint(1, 100)
 
-    if rand_val == 1:
-        await message.reply("YOU REALLY NEED TO SHUT UP!")
-        print("The bot said YOU REALLY NEED TO SHUT UP!")
+        if rand_val == 1:
+            await message.reply("YOU REALLY NEED TO SHUT UP!")
+            logger.info(f'Hit 1% trigger')
+            triggered = True
 
-    elif rand_val <= 11:
-        await message.reply("Shut up!")
-        print("The bot said Shut up!")
+        elif rand_val <= 11:
+            await message.reply("Shut up!")
+            logger.info(f'Hit 10% trigger')
+            triggered = True
+
+    if triggered:
+        user_spam_tracker[message.author.id] = current_time
 
 @client.tree.command(name="shut", description="Tells you to shut up!")
+@app_commands.checks.cooldown(1, 5.0, key=lambda i: (i.guild_id, i.user.id))
 async def shutCommand(interaction: discord.Interaction):
     await interaction.response.send_message("Shut up!")
 
@@ -81,7 +126,7 @@ async def helpCommand(interaction: discord.Interaction):
 
     embed.add_field(
         name="Privacy Policy",
-        value="I don't collect your data. LMAO. XD. Fixing commit errors right now.",
+        value="I don't collect your data, dw.",
         inline=False
     )
 
@@ -90,4 +135,7 @@ async def helpCommand(interaction: discord.Interaction):
 
 
 token = os.getenv("TOKEN")
-client.run(token)
+if not token:
+    logger.critical("TOKEN not found in environment variables.")
+else:
+    client.run(token)
